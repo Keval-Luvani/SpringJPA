2e8d9b6339c02e210bb232b457f635abcc65d155
Keval-Luvani <keval.webosmotic@gmail.com>
2023-03-02 15:43:50 +0530

    EmployeeDao,employeeDaoImpl fix

--- src/main/java/com/keval/SpringJPA/dao/EmployeeDao.java
+++ src/main/java/com/keval/SpringJPA/dao/EmployeeDao.java
@@ -1,54 +1,16 @@
 package com.keval.SpringJPA.dao;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import com.keval.SpringJPA.model.Employee;
 import com.keval.SpringJPA.model.Skill;
 
-@Component
-public class EmployeeDao {
 
-	@Autowired
-	EmployeeRepository employeeRepository;
-	@Autowired
-	SkillRepository skillRepository;
-	
-	public List<Employee> getEmployees() {
-		List<Employee> employeeList = new ArrayList<>();
-		employeeList = employeeRepository.findAll();
-		for(Employee employee:employeeList) {
-			employee.setSkillList(Arrays.asList(employee.getSkill().toString().replace("[","").replace("]","").replace(", ",",").split(",")));
-		}
-		return employeeList;
-	}
-
-	public Employee getEmployee(int employeeId) {
-		Optional<Employee> optionalEmployee = employeeRepository.findById(employeeId);
-		Employee employee = optionalEmployee.get();
-		employee.setSkillList(Arrays.asList(employee.getSkill().toString().replace("[","").replace("]","").replace(", ",",").split(",")));
-		return employee;
-	}
-	
-	public void addEmployee(Employee employee) {
-		employeeRepository.save(employee); 
-	}
-	
-	public void deleteEmployee(int employeeId) {
-		employeeRepository.deleteById(employeeId);
-	}
-
-	public void updateEmployee(Employee employee) {
-		List<Skill> skillList =  skillRepository.findByEmployee(employee);
-		employeeRepository.save(employee);
-		deleteskill(skillList);
-	}
-
-	public void deleteskill(List<Skill> skillList) {
-			skillRepository.deleteAll(skillList);
-	}
+public interface EmployeeDao {
+	public List<Employee> getEmployees();
+	public Employee getEmployee(int employeeId);
+	public void addEmployee(Employee employee);
+	public void deleteEmployee(int employeeId);
+	public void updateEmployee(Employee employee);
+	public void deleteSkills(List<Skill> skillList);
 }
